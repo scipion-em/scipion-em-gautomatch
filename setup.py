60967d87063474404438e59332cff952cff8ea81
Yunior C. Fonseca Reyna <fonsecareyna82@gmail.com>
2019-03-01 11:09:40 +0100

    Include protocol.conf to create the source distribution

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.3',  # Required
+    version='1.0.4',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -162,9 +162,9 @@ setup(
     # If using Python 2.6 or earlier, then these have to be included in
     # MANIFEST.in as well.
     # include_package_data=True,
-    #package_data={  # Optional
-    #   'gctf': ['gctf_logo.jpg'],
-    #},
+    package_data={  # Optional
+      'gautomatch': ['protocols.conf'],
+    },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
