9a94337bfdd054541852155bb93f8039abb168a6
Ratolon <miceta@cnb.csic.es>
2025-01-30 17:07:28 +0100

    sos 2

--- gautomatch/__init__.py
+++ gautomatch/__init__.py
@@ -60,9 +60,9 @@ class Plugin(pwem.Plugin):
         # cls._defineEmVar(GAUTOMATCH_ENV_ACTIVATION, DEFAULT_ACTIVATION_CMD,
         #                  description='Gautomatch environment activation command',
         #                  var_type=VarTypes.STRING)
-        # cls._defineEmVar(GAUTOMATCH_ENV_ACTIVATION, f'conda activate gautomatch',
-        #                  description='Gautomatch environment activation command',
-        #                  var_type=VarTypes.STRING)
+        cls._defineEmVar(GAUTOMATCH_ENV_ACTIVATION, f'conda activate gautomatch',
+                         description='Gautomatch environment activation command',
+                         var_type=VarTypes.STRING)
 
     @classmethod
     def getEnviron(cls):
